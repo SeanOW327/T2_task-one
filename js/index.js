b61998d96650c4f5f22455edae04106c74f45760
SeanOW327 <125765417+SeanOW327@users.noreply.github.com>
2023-05-02 13:05:23 +0200

    complete homework task
    create formulas in javascript

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// Exercise One
+
+let firstName = "Charlotte";
+let surname = "Evergarden";
+let age = 23;
+const subject = "development";
+let greeter = "Hello, my name is " + firstName + " " + surname + ", I am " + age + " years old and I am studying " + subject;
+
+console.log(greeter);
+
+
+// Exercise Two
+
+let numOne = 1;
+let numTwo = 2;
+let numThree = 3;
+let numFour = 4;
+let answer = 0;
+
+answer = numOne + numTwo / numThree * numFour;
+
+console.log(answer);
+
+
+// Exercise Three
+
+let timmy = 10;
+let susan = 25;
+let taxTotal = (10 / 100 * 15) + (25 / 100 * 26);
+
+console.log(taxTotal);
+
+
+// Exercise Four
+
+let john = 64;
+let tim = 78;
+let alfie = 45;
+let gwen = 95;
+
+let testAverage = (john + tim + alfie + gwen)/4 + "/100";
+
+console.log(testAverage);
+
+john = john + 7;
+tim = tim + 7;
+alfie = alfie + 7;
+gwen = gwen + 7;
+
+testAverage = (john + tim + alfie + gwen)/4 + "/100";
+
+console.log(testAverage);
